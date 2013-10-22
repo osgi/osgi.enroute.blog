6c2d8fdabef07c0be9c8fd0dc3766b006c7172a0
Peter Kriens <peter.kriens@aqute.biz>
2013-10-22 13:33:33 +0200

    The GUI for the blog

--- osgi.enroute.blog.appl/static/enroute/blog/js/blog.js
+++ osgi.enroute.blog.appl/static/enroute/blog/js/blog.js
@@ -19,26 +19,145 @@
 				+ "tea. 'I don't see any wine,' she remarked.\n'There isn't any,' said the March Hare.\n'Then it wasn't very civil of you to offer it,' said Alice angrily."
 	};
 
+	// Configure the routing table, this dispatches the page requests
+
+	MODULE.config(function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl : '/enroute/blog/htm/home.htm',
+			controller : Home
+		});
+		$routeProvider.when('/post/:id', {
+			templateUrl : '/enroute/blog/htm/post/view.htm',
+			controller : PostView
+		});
+		$routeProvider.when('/edit', {
+			templateUrl : '/enroute/blog/htm/post/edit.htm',
+			controller : PostNew
+		});
+		$routeProvider.when('/edit/:id', {
+			templateUrl : '/enroute/blog/htm/post/edit.htm',
+			controller : PostEdit
+		});
+		$routeProvider.when('/search', {
+			templateUrl : '/enroute/blog/htm/post/search.htm', // ?query=<query>&from=<time>&limit=<limit>
+			controller : PostSearch
+		});
+		$routeProvider.otherwise({
+			redirectTo : '/'
+		});
+	});
+
+	/**
+	 * The controller for the home screen. We fetch the current posts.
+	 * 
+	 * @param posts
+	 *            The promise for a list to hold the posts
+	 */
+	function Home($scope) {
+		$scope.posts = [ EXAMPLE ]
+	}
+
+	/**
+	 * Search posts page.
+	 * 
+	 * @param query
+	 *            The query string
+	 * @param from
+	 *            The from time (for paging)
+	 * @param limit
+	 *            the number of items
+	 */
+
+	function PostSearch($scope, $routeParams) {
+	}
+
+	/**
+	 * Show a post.
+	 * 
+	 * @param id
+	 *            From the uri
+	 * @param post
+	 *            Promise for the current post
+	 * @param edit
+	 *            Function to start the edit of this post
+	 * @param delete_
+	 *            Function to delete this post (needs confirmation)
+	 */
+
+	function PostView($scope, $routeParams, $location) {
+		$scope.post = EXAMPLE;
+
+		$scope.edit = function() {
+			$location.url("/edit/" + $routeParams.id);
+		}
+
+		$scope.delete_ = function() {
+			if (confirm("You are sure you want to delete this post?"))
+				$location.url("/");
+		}
+	}
+
+	/**
+	 * Edit a post (either new or existing)
+	 * 
+	 * @param save
+	 *            Function to save the edited post
+	 * @param reset
+	 *            Function to rest the edited post
+	 * @param cancel
+	 *            Function to goto the home page
+	 * @param isPristine
+	 *            true if not changed
+	 */
+
+	function editPost($scope, $location, value) {
+
+		$scope.save = function() {
+			$location.url("/post/" + 1);
+		}
+
+		$scope.reset = function() {
+			$scope.edit = angular.copy(value);
+		}
+
+		$scope.cancel = function() {
+			$location.url("/");
+		}
+
+		$scope.isPristine = function() {
+			return angular.equals($scope.edit, value);
+		}
+
+		$scope.reset();
+	}
+
+	/**
+	 * Edit New Post Controller
+	 * 
+	 * @see editPost
+	 */
+	function PostNew($scope, $location) {
+		editPost($scope, $location, {
+			content : "",
+			title : ""
+		});
+	}
+
+	/**
+	 * Edit Existing Post Controller
+	 * 
+	 * @see editPost
+	 */
+	function PostEdit($scope, $routeParams, $location) {
+		editPost($scope, $location, EXAMPLE);
+	}
+
 	/**
 	 * Main controller. Initialize the communication, etc.
 	 * 
 	 */
 
-	window.enBlog = function($scope, $location, $route, $resource) {
-		$scope.posts = []
-
-		$scope.save = function() {
-			$scope.posts.push({
-				title : $scope.title,
-				content : $scope.content,
-				created : new Date().getTime()
-			});
-			$scope.title = $scope.content = "";
-		}
-
-		$scope.remove = function(post) {
-			$scope.posts.splice($scope.posts.indexOf(post), 1);
-		}
+	window.enBlog = function($scope, $location, $resource, $route) {
 
 		/*
 		 * Error handler, sets $scope.message
@@ -55,12 +174,22 @@
 			command : '@command'
 		});
 
+		$scope.go = function(url) {
+			$location.url(url);
+		}
+
+		$scope.search = function(query) {
+			$location.url("/search");
+			$location.search({
+				query : query
+			});
+		}
+
 		$scope.testdata = function() {
-			$scope.post = Command.get({
+			Command.get({
 				command : 'TESTDATA'
-			}, function(post) {
-				$scope.title = post.title;
-				$scope.content = post.content;
+			}, function() {
+				$route.reload();
 				ok();
 			}, error);
 		}
@@ -70,6 +199,22 @@
 				command : 'EXCEPTION'
 			}, ok, error);
 		}
+
 	}
+
+	/**
+	 * Add a filter that truncates a text. This is useful when you have a table
+	 * and only want to show the first lines of text.
+	 */
+	MODULE.filter('truncate', function() {
+		return function(input, l) {
+			if (!input)
+				return '';
+			else if (l > input.length)
+				return input;
+			else
+				return input.substring(0, l)
+		}
+	});
 
 })(angular)
